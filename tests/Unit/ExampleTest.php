3e454c8fadd0f5bfb6269c74416172e1ac916c4c
aybjax <aybat93@gmail.com>
2021-02-05 15:11:34 +0600

    failing test added

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,4 +15,14 @@ class ExampleTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testFailBasicTest()
+    {
+        $this->assertTrue(false);
+    }
 }
