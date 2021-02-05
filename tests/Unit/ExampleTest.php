047cef23938d88e2172540be9d823f5530abc65c
aybjax <aybat93@gmail.com>
2021-02-05 15:16:01 +0600

    Fail added again?

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -21,7 +21,7 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testFailBasicTest()
+    public function testFailTest()
     {
         $this->assertTrue(false);
     }
